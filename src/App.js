f1dece5ef08856d70e25a1c035e2169f134ff3f1
Phoenix314159 <derivative7117@live.com>
2019-04-06 20:31:12 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [state, setData] = useState({data: [], hasDataLoaded: false})
+
+  useEffect(() => {
+    (async () => {
+      const {data} = await axios.get('/api/getdata')
+      setData({data, hasDataLoaded: true})
+    })()
+  }, [state])
+
+  const renderData = () => {
+    const{data} = state
+    return data.map((person) => {
+      const {id, name, age} = person
+      return (
+        <div key={id}>
+          <h3>{name}</h3>
+          <h3>{age}</h3>
+        </div>
+      )
+    })
   }
+  const {hasDataLoaded} = state
+  if (!hasDataLoaded) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          {renderData()}
+        </div>
+      </header>
+    </div>
+  )
+
 }
 
-export default App;
+export default App
